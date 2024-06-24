62fe7c58d201d2a708a9b9b27efd3b3226ffea8d
John Nicholas Suharjono <john.suharjono10@gmail.com>
2024-06-25 01:22:25 +0700

    complete landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,141 +1,334 @@
 import { MaxWidthWrapper } from '@/components/max-width-wrapper'
 import { Phone } from '@/components/phone'
-import { CheckIcon, StarFilledIcon } from '@radix-ui/react-icons'
+import { Samples } from '@/components/sample'
+import { buttonVariants } from '@/components/ui/button'
+import { Icons } from '@/components/ui/icons'
+import {
+	ArrowRightIcon,
+	CheckIcon,
+	StarFilledIcon,
+} from '@radix-ui/react-icons'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
 	return (
 		<div className='bg-slate-50'>
-			<MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
-				<div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
-					<div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
-						<div className='absolute w-28 left-0 -top-20 hidden lg:block'>
-							<div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
-							<Image src='/snake-1.png' width={112} height={112} alt='Snake' />
-						</div>
-						<h1 className='relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
-							Your Image on a{' '}
-							<span className='bg-blue-600 px-2 text-white'>Custom</span> Phone
-							Case
-						</h1>
-						<p className='mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap'>
-							Capture your favorite memories with your own,{' '}
-							<span className='font-semibold'>one-of-one</span> phone case. Case
-							Cobra allows you to protect your memories, not just your phone
-							case.
-						</p>
-
-						<ul className='mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start'>
-							<div className='space-y-2'>
-								<li className='flex gap-1.5 items-center text-left'>
-									<CheckIcon className='h-5 w-5 shrink-0 text-blue-600' />
-									High-quality, durable material
-								</li>
-								<li className='flex gap-1.5 items-center text-left'>
-									<CheckIcon className='h-5 w-5 shrink-0 text-blue-600' />5
-									years print guarantee
-								</li>
-								<li className='flex gap-1.5 items-center text-left'>
-									<CheckIcon className='h-5 w-5 shrink-0 text-blue-600' />
-									Modern iPhone models supported
-								</li>
-							</div>
-						</ul>
-
-						<div className='mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5'>
-							<div className='flex -space-x-4'>
+			<section>
+				<MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
+					<div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
+						<div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
+							<div className='absolute w-28 left-0 -top-20 hidden lg:block'>
+								<div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
 								<Image
-									height={40}
-									width={40}
-									className='inline-block rounded-full ring-2 ring-slate-100'
-									src={'/users/user-1.png'}
-									alt='user image'
-								/>
-								<Image
-									height={40}
-									width={40}
-									className='inline-block rounded-full ring-2 ring-slate-100'
-									src={'/users/user-2.png'}
-									alt='user image'
-								/>
-								<Image
-									height={40}
-									width={40}
-									className='inline-block rounded-full ring-2 ring-slate-100'
-									src={'/users/user-3.png'}
-									alt='user image'
-								/>
-								<Image
-									height={40}
-									width={40}
-									className='inline-block rounded-full ring-2 ring-slate-100'
-									src={'/users/user-4.jpg'}
-									alt='user image'
-								/>
-								<Image
-									height={40}
-									width={40}
-									className='inline-block rounded-full ring-2 ring-slate-100 object-cover'
-									src={'/users/user-5.jpg'}
-									alt='user image'
+									src='/snake-1.png'
+									width={112}
+									height={112}
+									alt='Snake'
 								/>
 							</div>
-
-							<div className='flex flex-col justify-between items-center sm:items-start'>
-								<div className='flex gap-0.5'>
-									<StarFilledIcon
-										height={16}
-										width={16}
-										className='text-blue-600'
+							<h1 className='relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
+								Your Image on a{' '}
+								<span className='bg-blue-600 px-2 md:px-3 text-white rounded-2xl'>
+									Custom
+								</span>{' '}
+								Phone Case
+							</h1>
+							<p className='mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap'>
+								Capture your favorite memories with your own,{' '}
+								<span className='font-semibold'>one-of-one</span> phone case.
+								Case Cobra allows you to protect your memories, not just your
+								phone case.
+							</p>
+							<ul className='mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start'>
+								<div className='space-y-2'>
+									<li className='flex gap-1.5 items-center text-left'>
+										<CheckIcon className='h-5 w-5 shrink-0 text-blue-600' />
+										High-quality, durable material
+									</li>
+									<li className='flex gap-1.5 items-center text-left'>
+										<CheckIcon className='h-5 w-5 shrink-0 text-blue-600' />5
+										years print guarantee
+									</li>
+									<li className='flex gap-1.5 items-center text-left'>
+										<CheckIcon className='h-5 w-5 shrink-0 text-blue-600' />
+										Modern iPhone models supported
+									</li>
+								</div>
+							</ul>
+							<div className='mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5'>
+								<div className='flex -space-x-4'>
+									<Image
+										height={40}
+										width={40}
+										className='inline-block rounded-full ring-2 ring-slate-100'
+										src={'/users/user-1.png'}
+										alt='user image'
 									/>
-									<StarFilledIcon
-										height={16}
-										width={16}
-										className='text-blue-600'
+									<Image
+										height={40}
+										width={40}
+										className='inline-block rounded-full ring-2 ring-slate-100'
+										src={'/users/user-2.png'}
+										alt='user image'
 									/>
-									<StarFilledIcon
-										height={16}
-										width={16}
-										className='text-blue-600'
+									<Image
+										height={40}
+										width={40}
+										className='inline-block rounded-full ring-2 ring-slate-100'
+										src={'/users/user-3.png'}
+										alt='user image'
 									/>
-									<StarFilledIcon
-										height={16}
-										width={16}
-										className='text-blue-600'
+									<Image
+										height={40}
+										width={40}
+										className='inline-block rounded-full ring-2 ring-slate-100'
+										src={'/users/user-4.jpg'}
+										alt='user image'
 									/>
-									<StarFilledIcon
-										height={16}
-										width={16}
-										className='text-blue-600'
+									<Image
+										height={40}
+										width={40}
+										className='inline-block rounded-full ring-2 ring-slate-100 object-cover'
+										src={'/users/user-5.jpg'}
+										alt='user image'
 									/>
 								</div>
-
-								<p>
-									<span className='font-semibold'>1.250</span> happy customers
-								</p>
+								<div className='flex flex-col justify-between items-center sm:items-start'>
+									<div className='flex gap-0.5'>
+										<StarFilledIcon
+											height={16}
+											width={16}
+											className='text-blue-600'
+										/>
+										<StarFilledIcon
+											height={16}
+											width={16}
+											className='text-blue-600'
+										/>
+										<StarFilledIcon
+											height={16}
+											width={16}
+											className='text-blue-600'
+										/>
+										<StarFilledIcon
+											height={16}
+											width={16}
+											className='text-blue-600'
+										/>
+										<StarFilledIcon
+											height={16}
+											width={16}
+											className='text-blue-600'
+										/>
+									</div>
+									<p>
+										<span className='font-semibold'>1.250</span> happy customers
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-
-				<div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
-					<div className='relative md:max-w-xl'>
-						<img
-							alt='your image'
-							src='/your-image.png'
-							className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
-						/>
-
-						<img
-							src='/line.png'
-							alt='line'
-							className='absolute w-20 -left-6 -bottom-6 select-none'
-						/>
-
-						<Phone className='w-64' imgSrc='/testimonials/1.jpg' />
+					<div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
+						<div className='relative md:max-w-xl'>
+							<img
+								alt='your image'
+								src='/your-image.png'
+								className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
+							/>
+							<img
+								src='/line.png'
+								alt='line'
+								className='absolute w-20 -left-6 -bottom-6 select-none'
+							/>
+							<Phone className='w-64' imgSrc='/testimonials/1.jpg' />
+						</div>
 					</div>
+				</MaxWidthWrapper>
+			</section>
+
+			{/* Value proposition section */}
+			<section className='bg-slate-100 py-24'>
+				<MaxWidthWrapper className='flex flex-col items-center gap-16 sm:gap-32'>
+					<div className='flex flex-col lg:flex-row items-center gap-4 sm:gap-6'>
+						<h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+							What our{' '}
+							<span className='relative px-2'>
+								customers{' '}
+								<Icons.underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-8 text-blue-500' />
+							</span>{' '}
+							say
+						</h2>
+
+						<Image
+							src='/snake-2.png'
+							alt=''
+							width={96}
+							height={96}
+							className='order-0 lg:order-2'
+						/>
+					</div>
+
+					<div className='mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
+						<div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+							<div className='flex gap-0 5 mb-2'>
+								{Array.from({ length: 5 }).map((_, i) => (
+									<StarFilledIcon
+										key={i}
+										height={20}
+										width={20}
+										className='text-blue-600'
+									/>
+								))}
+							</div>
+
+							<div className='text-lg leading-8'>
+								<p>
+									"The case feels durable and I even got a compliment on the
+									design. Had the case for two and half months now and{' '}
+									<span className='p-0.5 bg-slate-800 text-white'>
+										the image is super clear
+									</span>
+									, on the case I had before the image started fading into
+									yellow-ish color after a couple weeks. Love it"
+								</p>
+							</div>
+
+							<div className='flex gap-4 mt-2'>
+								<Image
+									src='/users/user-1.png'
+									alt='user image'
+									width={48}
+									height={48}
+									className='rounded-full'
+									objectFit='cover'
+								/>
+								<div className='flex flex-col'>
+									<p className='font-semibold'>Jonathan</p>
+									<div className='flex gap-1.5 items-center text-zinc-600'>
+										<CheckIcon className='h-4 w-4 stroke-[3px] text-blue-600' />
+										<p className='text-sm'>Verified Purchase</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+							<div className='flex gap-0 5 mb-2'>
+								{Array.from({ length: 5 }).map((_, i) => (
+									<StarFilledIcon
+										key={i}
+										height={20}
+										width={20}
+										className='text-blue-600'
+									/>
+								))}
+							</div>
+
+							<div className='text-lg leading-8'>
+								<p>
+									"I usually keep my phone together with my keys in my pocket
+									and that led to some pretty heavy scratchmarks on all of my
+									last phone cases. This one has been holding up for a month now
+									and{' '}
+									<span className='p-0.5 bg-slate-800 text-white'>
+										it still looks brand new
+									</span>
+									. I dig it"
+								</p>
+							</div>
+
+							<div className='flex gap-4 mt-2'>
+								<Image
+									src='/users/user-4.jpg'
+									alt='user image'
+									width={48}
+									height={48}
+									className='rounded-full'
+									objectFit='cover'
+								/>
+								<div className='flex flex-col'>
+									<p className='font-semibold'>Josh</p>
+									<div className='flex gap-1.5 items-center text-zinc-600'>
+										<CheckIcon className='h-4 w-4 stroke-[3px] text-blue-600' />
+										<p className='text-sm'>Verified Purchase</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</MaxWidthWrapper>
+
+				<div className='pt-16'>
+					<Samples />
 				</div>
-			</MaxWidthWrapper>
+			</section>
+
+			<section>
+				<MaxWidthWrapper className='py-24'>
+					<div className='mb-12 px-6 lg:px-8'>
+						<div className='mx-auto max-w-2xl sm:text-center'>
+							<h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+								Upload your photo and get{' '}
+								<span className='relative px-2 bg-blue-600 text-white'>
+									your own case
+								</span>{' '}
+								now
+							</h2>
+						</div>
+					</div>
+
+					<div className='mx-auto max-w-6xl px-6 lg:px-8'>
+						<div className='relative flex flex-col items-center md:grid grid-cols-2 gap-40'>
+							<img
+								src='/arrow.png'
+								className='absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0'
+							/>
+
+							<div className='relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl'>
+								<img
+									src='/horse.jpg'
+									className='rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full'
+								/>
+							</div>
+
+							<Phone className='w-60' imgSrc='/horse_phone.jpg' />
+						</div>
+					</div>
+
+					<ul className='mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit'>
+						<li className='w-fit'>
+							<CheckIcon className='h-5 w-5 text-blue-600 inline mr-1.5' />
+							High-quality silicone material
+						</li>
+						<li className='w-fit'>
+							<CheckIcon className='h-5 w-5 text-blue-600 inline mr-1.5' />
+							Scratch- and fingerprint resistant coating
+						</li>
+						<li className='w-fit'>
+							<CheckIcon className='h-5 w-5 text-blue-600 inline mr-1.5' />
+							Wireless charging compatible
+						</li>
+						<li className='w-fit'>
+							<CheckIcon className='h-5 w-5 text-blue-600 inline mr-1.5' />5
+							year print warranty
+						</li>
+
+						<div className='flex justify-center'>
+							<Link
+								className={buttonVariants({
+									size: 'lg',
+									className: 'mx-auto mt-8',
+								})}
+								href='/configure/upload'
+							>
+								Create your case now{' '}
+								<ArrowRightIcon className='h-4 w-4 ml-1.5' />
+							</Link>
+						</div>
+					</ul>
+				</MaxWidthWrapper>
+			</section>
 		</div>
 	)
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,11 +2,14 @@ import { MaxWidthWrapper } from '@/components/max-width-wrapper'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
 import { ArrowRightIcon } from '@radix-ui/react-icons'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
-export function Navbar() {
-	// TODO: User-logic
-	const user = undefined
-	const isAdmin = true
+export async function Navbar() {
+	const { getUser } = getKindeServerSession()
+	const user = await getUser()
+
+	const isAdmin = user?.email === process.env.ADMIN_EMAIL
+
 	return (
 		<nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
 			<MaxWidthWrapper>
@@ -26,7 +29,7 @@ export function Navbar() {
 								</Link>
 								{isAdmin ? (
 									<Link
-										href='/api/auth/logout'
+										href=''
 										className={buttonVariants({ size: 'sm', variant: 'ghost' })}
 									>
 										Dashboard
@@ -65,7 +68,7 @@ export function Navbar() {
 										className: 'hidden sm:flex items-center gap-1',
 									})}
 								>
-									Create Case <ArrowRightIcon className='ml-1.5 h-5 w-5' />
+									Create Case <ArrowRightIcon className='h-5 w-5' />
 								</Link>
 							</>
 						)}
